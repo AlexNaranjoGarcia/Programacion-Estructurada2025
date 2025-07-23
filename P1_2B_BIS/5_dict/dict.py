6ecb66d9450a51604c40b726ab99f3be8d699dbb
AlexNaranjoGarcia <marko_3141240241@utd.edu.mx>
2025-07-23 07:34:56 -0700

    Delete P1_2B_BIS directory

--- P1_2B_BIS/5_dict/dict.py
+++
@@ -1,9 +0,0 @@
-"""" 
-dict.-
-Es un tipo de datos que se utiliza para almacenar datos de diferente tipo de datos pero en lugar de tener como las listas indices numericos
- tiene alfanumericos. Es decir es algo parecido como los Objetos
-
-Tambien se conoce como arreglo asociativo u Objeto JSON
-
-El diccionario es una collecion ordenada y modificable. no hay miembros duplicados.
-"""
